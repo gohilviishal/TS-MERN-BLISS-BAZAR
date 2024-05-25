3d957ec9696c95a2d48c0b5f563336e68e4dc515
gohilviishal <viishalgohil193@gmail.com>
2024-05-25 15:30:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typescript MERN BLISS-BAZAR
+
+#Lessons
+1. Introduction
+2. Install tools
+3. Create typescript react app by vite
+4. Create git repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>TS BLISS BAZAR</header>
+      <main></main>
+      <footer>All rights reserved </footer>
+    </div>
+  )
+}
+
+export default App
